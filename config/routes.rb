b7b258a76c6dee3004606f550e9da3c482ca91a1
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2020-02-29 00:54:17 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :authors, only: [:show]
+  # resources :authors, only: [:show]
 
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+  # resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
-  root 'posts#index'
+  # root 'posts#index'
   
   get 'authors/:id/posts'
   get 'authors/:id/posts/:post_id'
